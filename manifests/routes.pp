7a683e65efe393fb8be377d895db4bcbb187d691
dn1s <dominik.n.schuster@gmail.com>
2016-12-29 16:32:00 +0100

    Added new variable which stores interface names for file creation

--- manifests/routes.pp
+++ manifests/routes.pp
@@ -1,20 +1,25 @@
 # Wrapper to create route resources from hiera
 class puppet_network::routes (
   $routes=undef,
+  $interfaces=[],
 ) {
-  if $routes {
+  if $interfaces {
     file { '/etc/sysconfig/network-scripts':
       ensure => directory,
       owner  => root,
       group  => root,
       mode   => "0755",
     }
-    file { "/etc/sysconfig/network-scripts/route-$routes":
-      ensure  => file,
-      owner   => root,
-      group   => root,
-      mode    => "0644",
+    each($interfaces) |$interface| {
+      file { "/etc/sysconfig/network-scripts/route-$interface":
+        ensure => file,
+        owner  => root,
+        group  => root,
+        mode   => "0644",
+      }
     }
-    create_resources('puppet_network::create_route', $routes)
+    if $routes {
+      create_resources('puppet_network::create_route', $routes)
+    }
   }
 }
