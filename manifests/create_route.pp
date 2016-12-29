9a412f41df753fa5e186d3b50e867c7fd0560bcb
dn1s <dominik.n.schuster@gmail.com>
2016-12-29 17:28:50 +0100

    Removed variable for netmask because can just extend network variable like 10.10.0.0/15

--- manifests/create_route.pp
+++ manifests/create_route.pp
@@ -2,19 +2,17 @@
 #  puppet_network::create_route { 'eth0_10.12.0.0_16':
 #    gateway   => '172.17.10.1',
 #    interface => 'eth0',
-#    netmask   => '16',
-#    network   => '10.12.0.0',
+#    network   => '10.12.0.0/16',
 #  }
 define puppet_network::create_route (
   $gateway,
   $interface,
   $network = 'default',
-  $netmask = undef,
 ) {
   if $netmask {
     file_line { "route-$interface-$network":
       path    => "/etc/sysconfig/network-scripts/route-$interface",
-      content => "$network/$netmask via $gateway dev $interface",
+      content => "$network via $gateway dev $interface",
     }
   }
   else {
